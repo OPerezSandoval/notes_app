a489aebbfcc0ee07010cede582e82d735251e135
OPEREZSANDOVAL <PEREZ.OMAR99@YAHOO.COM>
2024-10-05 11:51:47 -0500

    Initial Skeleton

---
+++ src/test/java/notes_app/demo/NotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package notes_app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
